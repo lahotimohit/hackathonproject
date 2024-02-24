5dfda0636c5674a13ae05500b6522a615a0722ee
Mohit Lahoti <engtechno25@gmail.com>
2024-02-24 16:02:06 +0530

    Implementation of react with django done

--- backapp/views.py
+++ backapp/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def example_view(request):
+    data = {"message": "Hello from Django!"}
+    return JsonResponse(data)
--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backapp',
-    'taggit'
+    'taggit',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -24,9 +25,14 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'backend.urls'
